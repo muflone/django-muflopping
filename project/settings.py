837fb3c6f32bf5d46ff2be32d08aa7fdaf886de9
Muflone <webreg@muflone.com>
2026-06-13 23:44:58 +0200

    New application accounts

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
