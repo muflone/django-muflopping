3ad51264fd07004092b2164ca414ffd7286b43f5
Muflone <webreg@muflone.com>
2026-06-27 17:24:07 +0200

    Added static and media files for debug mode

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,8 @@
 ##
 
 import django.conf
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -40,3 +42,9 @@ urlpatterns = [
          view=SpectacularSwaggerView.as_view(),
          name='swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(prefix=settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
