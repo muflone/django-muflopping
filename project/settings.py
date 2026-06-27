92ee9baa1e884387b52db21a2b56d07eb7da5f2b
Muflone <webreg@muflone.com>
2026-06-27 18:15:27 +0200

    Added STATIC_ROOT

--- project/settings.py
+++ project/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.parent / 'static'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.parent / 'media'
 
